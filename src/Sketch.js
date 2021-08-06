206209aac857444801e884badcc6b54760ae9743
KeslieBox <kesliebox@gmail.com>
2021-08-06 14:48:37 -0500

    sets up Paddle Component and canvas for game

--- src/Sketch.js
+++ src/Sketch.js
@@ -1,96 +1,94 @@
-import React from 'react'
-import p5 from 'p5'
-import Paddle from './paddle'
+// import React from 'react'
+// import p5 from 'p5'
+// import Paddle from './paddle'
 
-let pad 
+// let pad 
 
-class Sketch extends React.Component {
-    constructor(props) {
-        super(props)
-        this.myRef = React.createRef()
-        this.state = {
-            paddle: ''
-        }
-    }
+// class Sketch extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.myRef = React.createRef()
+//         this.state = {
+//             paddle: ''
+//         }
+//     }
 
-    Sketch = (p) => {
-        let canvas, paddle, paddle2
+//     Sketch = (p) => {
+//         let canvas, paddle, paddle2
 
-        p.setup = () => {
-            canvas = p.createCanvas(800,600)
-        }
+//         p.setup = () => {
+//             canvas = p.createCanvas(800,600)
+//         }
 
-        const setup = (p5, canvasParentRef) => {
-            p5.createCanvas(500, 400).parent(canvasParentRef)
-          }
+//         const setup = (p5, canvasParentRef) => {
+//             p5.createCanvas(500, 400).parent(canvasParentRef)
+//           }
 
-        p.draw = () => {
-            p.background(20)
-            p.rect(p.width / 2.5, p.height - 40, 150, 20, 20)
+//         p.draw = () => {
+//             p.background(20)
+//             p.rect(p.width / 2.5, p.height - 40, 150, 20, 20)
             
-        }
+//         }
 
-        //p5 event on key press
-        p.keyPressed = (e) => {
-            // working, but you have to keep pressing key for each move
-            if(e.key === 'ArrowRight'){
-                p.moveRight(e)
-            } else if (e.key === 'ArrowLeft') {
-                p.moveLeft(e)
-            }
+//         //p5 event on key press
+//         p.keyPressed = (e) => {
+//             // working, but you have to keep pressing key for each move
+//             // maybe try while loop?? or recursion
+            
+//             if(e.key === 'ArrowRight'){
+//                 p.moveRight(e)
+//             } else if (e.key === 'ArrowLeft') {
+//                 p.moveLeft(e)
+//             }
 
-            // not working:
-            // if(e.key === 'ArrowRight'){
-            //     this.setState(prevState => {
-            //         // return {...prevState, paddle: paddle.width + 20}
-            //         if (prevState.paddle){
-            //             debugger
-            //             return {...prevState, paddle: p.rect(p.width +=20, p.height - 40, 150, 20, 20)}
-            //         } else {
-            //             debugger
-            //             return {paddle: p.rect(p.width, p.height - 40, 150, 20, 20)}
-            //         }
-            //     })
-            // }
-        }
-
-        p.moveRight = (e) => {
-            // how to make consistent movement while key pressed?
-            // if (p.width > 1){
-            debugger
-            p.width += 20
-            // }
            
-        }
+//             // not working, but need something similar:
+//             // if(e.key === 'ArrowRight'){
+//             //     this.setState(prevState => {
+//                     // return {...prevState, paddle: paddle.width + 20}
+//             //         if (prevState.paddle){
+//             //             debugger
+//             //             return {...prevState, paddle: p.rect(p.width +=20, p.height - 40, 150, 20, 20)}
+//             //         } else {
+//             //             debugger
+//             //             return {paddle: p.rect(p.width, p.height - 40, 150, 20, 20)}
+//             //         }
+//             //     })
+//             // }
+//         }
 
-        p.moveLeft = (e) => {
-            console.log('something')
-            paddle = p.rect(p.width -= 20)
-            
-        }
-    }
+//         p.moveRight = (e) => {
+//             // how to make consistent movement while key pressed?
+//             p.width += 20
+           
+//         }
 
-    componentDidMount() {
-        this.myP5 = new p5(this.Sketch, this.myRef.current)
-    }
+//         p.moveLeft = (e) => {
+//             p.width -= 20    
+//         }
+//     }
 
-    componentDidUpdate() {
-        this.myP5.remove()
-        this.myP5 = new p5(this.Sketch, this.myRef.current)
-    }
+//     componentDidMount() {
+//         this.myP5 = new p5(this.Sketch, this.myRef.current)
+//     }
 
-    componentWillUnmount() {
-        this.myP5.remove()
-    }
+//     componentDidUpdate() {
+//         this.myP5.remove()
+//         this.myP5 = new p5(this.Sketch, this.myRef.current)
+//     }
 
-    render() {
-        return (
-            //This div will contain our p5 sketch
-            <div ref={this.myRef}>
-            </div>
-        )
-    }
-}
+//     componentWillUnmount() {
+//         this.myP5.remove()
+//     }
 
-export default Sketch;
+//     render() {
+//         return (
+//             //This div will contain our p5 sketch
+//             <div ref={this.myRef}>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Sketch;
 
--- src/paddle.js
+++ src/paddle.js
@@ -2,36 +2,42 @@ import { Component } from "react"
 import React from 'react'
 import p5 from 'p5'
 
-const speed = 8
 class Paddle extends Component{
     constructor(props) {
       super(props)
       this.myRef = React.createRef()
-      this.state = {
-        width: 150,
-        height: 25,
-        // color: color(255),
-        location: React.createVector((this.state.width / 2) - (this.state.width / 2), this.state.height - 35),
-        speed: {
-          right: React.createVector(speed, 0),
-          left: React.createVector(speed * -1, 0)
-        },
+    }
+
+    handleKeyDown(e) {
+      if(e.key === 'ArrowRight'){
+          this.moveRight(e)
+      } else if (e.key === 'ArrowLeft') {
+          this.moveLeft(e)
       }
     }
-  
-    display() {
-      p5.fill(this.state.color)
-      p5.rect(this.state.location.x, this.state.location.y, this.state.width, this.state.height)
-    }
-  
-    move(direction) {
-      this.state.location.add(this.state.speed[direction])
-  
-      if(this.state.location.x < 0) {
-        this.state.location.x = 0
-      } else if(this.state.location.x + this.width > this.state.width) {
-        this.state.location.x = this.width - this.state.width   
+
+    moveLeft(e){
+      let leftNumbers = e.target.style.left.replace("px", "")
+      let left = parseInt(leftNumbers, 10)
+      if (left > 0) {
+          e.target.style.left = `${left - 25}px`
       }
+    }
+
+    moveRight(e){
+      let leftNumbers = e.target.style.left.replace("px", "")
+      let left = parseInt(leftNumbers, 10)
+      if (left <= 780) {
+          e.target.style.left = `${left + 25}px`
+      }
+    }
+
+    render (){
+      return (
+        <div className='container'>
+          <div id='paddle' tabIndex={-1} style={{bottom: 0, left: '400px'}} onKeyDown={(e) => this.handleKeyDown(e)}></div>
+        </div>
+      )
     }
   }
 
